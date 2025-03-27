0bd32f5e8fb419db52666eceaea245e3150da563
Vaahtoo✨ <39770424+vaahtoo@users.noreply.github.com>
2025-03-27 13:29:01 +0700

    Update admin.py

--- service/BioGuard/admin.py
+++ service/BioGuard/admin.py
@@ -1,9 +1,1 @@
-from django.contrib import admin
-from .models import UserProfile, Patient, Appointment, Service, Staff 
 
-
-admin.site.register(UserProfile)
-admin.site.register(Patient)
-admin.site.register(Appointment)
-admin.site.register(Service)
-admin.site.register(Staff)
